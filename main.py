7a890ee64576fd3be9e8ed50df33fb6440fde99b
Maxime Moreillon <m.moreillon@gmail.com>
2025-01-06 11:07:02 +0900

    simplified example

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.shell.tool import ShellTool
 from langchain_core.messages import HumanMessage
-from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 
 
 # Create the agent
-memory = MemorySaver()
 model = ChatOpenAI(model="gpt-4o-mini")
 search = ShellTool(ask_human_input=True)
 tools = [search]
-agent_executor = create_react_agent(model, tools, checkpointer=memory)
+agent_executor = create_react_agent(model, tools)
 
 # Use the agent
-config = {"configurable": {"thread_id": "abc123"}}
-for chunk in agent_executor.stream(
-    {"messages": [HumanMessage(content="Please find out what OS I am running")]}, config
-):
-    print(chunk)
-    print("----")
-
+response = agent_executor.invoke(
+    {"messages": [HumanMessage(content="What is my OS?")]}
+)
+print(response["messages"])
